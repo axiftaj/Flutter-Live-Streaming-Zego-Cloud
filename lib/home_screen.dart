487159ee0706751b81d9492a0da8305f11ecb49a
Asif Taj <47206155+axiftaj@users.noreply.github.com>
2022-11-15 14:03:41 +0500

    complete

---
+++ lib/home_screen.dart
@@ -0,0 +1,105 @@
+
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_live_streaming_app/live_sceen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  // Generate Live Streaming ID with 6 digit length
+  final liveIDController = TextEditingController(
+    text: '1111',
+  );
+
+  // Generate userID with 6 digit length
+  final String userID = Random().nextInt(900000 + 100000).toString();
+
+
+  @override
+  Widget build(BuildContext context) {
+    var buttonStyle = ElevatedButton.styleFrom(
+      backgroundColor: const Color(0xff034ada),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+    );
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Text('Your UserID: $userID'),
+            const Text('Please test with two or more devices'),
+            const SizedBox(
+              height: 30,
+            ),
+            TextFormField(
+              controller: liveIDController,
+              decoration: const InputDecoration(
+                labelText: 'Join or Start a Live by Input an ID',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: buttonStyle,
+              child: const Text('Start a Live'),
+              onPressed:(){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LiveScreenView(
+                      liveID: liveIDController.text,
+                      isHost: true, userID: '',
+                    ),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+                style: buttonStyle,
+                child: const Text('Join a Live'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LiveScreenView(
+                        liveID: liveIDController.text,
+                        isHost: false,
+                        userID: '',
+                      ),
+                    ),
+                  );
+                }
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+}
---
+++ lib/live_sceen.dart
@@ -0,0 +1,41 @@
+
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:zego_uikit_prebuilt_live_streaming/zego_uikit_prebuilt_live_streaming.dart';
+
+
+// Live Page Prebuilt UI from ZEGOCLOUD UIKits
+class LiveScreenView extends StatelessWidget {
+  final String liveID , userID;
+  final bool isHost;
+
+  LiveScreenView({
+    super.key,
+    required this.liveID,
+    this.isHost = false,
+    required this.userID
+  });
+
+  // Add your app id here and app sign in
+  // Make sure you replace with your own
+  final int appID = int.parse('ZEGO_APP_ID');
+  final String appSign = 'ZEGO_APP_ID';
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: ZegoUIKitPrebuiltLiveStreaming(
+        appID: appID,
+        appSign: appSign,
+        userID: userID,
+        userName: 'user_$userID',
+        liveID: liveID,
+        config: isHost
+            ? ZegoUIKitPrebuiltLiveStreamingConfig.host()
+            : ZegoUIKitPrebuiltLiveStreamingConfig.audience()
+          ..audioVideoViewConfig.showAvatarInAudioMode = true
+          ..audioVideoViewConfig.showSoundWavesInAudioMode = true,
+      ),
+    );
+  }
+}
