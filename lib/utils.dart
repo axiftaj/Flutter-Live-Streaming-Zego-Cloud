41ce912d763ef045fd74490d6b3f492a780ec2c6
Asif Taj <47206155+axiftaj@users.noreply.github.com>
2022-11-24 21:33:21 +0500

    one to one video calling added

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,6 +1,6 @@
 
 class Utils {
 
-  static int appId = 790657303 ;
-  static String appSignin = "77ae766ae21bfd905c46e36c34e925f9a324d036ce643d4331ffacd3bfdb1e34" ;
+  static int appId = 121212 ;
+  static String appSignin = "your sign in" ;
 }
